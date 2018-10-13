3a9b3f059f3fbb8081d2b85aac0e2cfdf59fef9a
Douglas Muraoka <douglas.muraoka@gmail.com>
2018-10-13 10:24:29 -0300

    feat: Add current logged user query

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,16 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID }
+    // Query that returns the currently logged user.
+    // If user is logged in, Passport will populate the req.user object.
+    user: { type: UserType },
+    resolve(parentValue, args, req) {
+      return req.user;
+    }
   }
 });
 
